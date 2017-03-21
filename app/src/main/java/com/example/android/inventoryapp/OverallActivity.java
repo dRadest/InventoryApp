a3d56f3c29e77e17a0d4cf60165a998fb34262dc
DRad <progdrad@gmail.com>
2017-03-21 08:59:06 +0100

    Hook up fab to open DetailActivity

--- app/src/main/java/com/example/android/inventoryapp/OverallActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/OverallActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.inventoryapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class OverallActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class OverallActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.overall_activity);
+
+        // setup fab to start detail activity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detailActivityIntent = new Intent(OverallActivity.this, DetailActivity.class);
+                startActivity(detailActivityIntent);
+            }
+        });
     }
 }
