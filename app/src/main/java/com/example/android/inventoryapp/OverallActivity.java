d556c518367df0b57444a92bbcb270a59075f3fc
DRad <progdrad@gmail.com>
2017-03-21 11:09:46 +0100

    Add menu items to OverallActivity

--- app/src/main/java/com/example/android/inventoryapp/OverallActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/OverallActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -31,6 +33,30 @@ public class OverallActivity extends AppCompatActivity {
         View emptyView = findViewById(R.id.empty_view);
         itemListView.setEmptyView(emptyView);
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_catalog.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.overall_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Insert dummy data" menu option
+            case R.id.insert_overall_menu_item:
+                return true;
+            // Respond to a click on the "Clear all" menu option
+            case R.id.clear_overall_menu_item:
+                return true;
+            // Respond to a click on the "Change view" menu option
+            case R.id.change_view:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
