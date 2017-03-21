3519623ece6eea804b5c21414c42f26cd31d4a1d
DRad <progdrad@gmail.com>
2017-03-21 15:34:35 +0100

    Implement method to enter dummy data

--- app/src/main/java/com/example/android/inventoryapp/OverallActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/OverallActivity.java
@@ -1,6 +1,10 @@
 package com.example.android.inventoryapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +12,22 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.android.inventoryapp.data.InventoryContract;
+import com.example.android.inventoryapp.data.InventoryDbHelper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.example.android.inventoryapp.data.InventoryContract.InventoryEntry;
 
 public class OverallActivity extends AppCompatActivity {
+
+    // InventoryDbHelper instance to preform CRUD operations
+    private InventoryDbHelper mDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +43,9 @@ public class OverallActivity extends AppCompatActivity {
                 startActivity(detailActivityIntent);
             }
         });
+
+        // instanciate mDbHelper
+        mDbHelper = new InventoryDbHelper(getApplicationContext());
 
         // find list view of items
         ListView itemListView = (ListView) findViewById(R.id.list_view);
@@ -49,6 +70,7 @@ public class OverallActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Insert dummy data" menu option
             case R.id.insert_overall_menu_item:
+                insertDummyItem();
                 return true;
             // Respond to a click on the "Clear all" menu option
             case R.id.clear_overall_menu_item:
@@ -59,4 +81,35 @@ public class OverallActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // helper method to insert dummy
+    public void insertDummyItem(){
+        // get writable database from mDbHelper
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // convert bitmap to byte array
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.hammer);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        // create content values
+        ContentValues values = new ContentValues();
+        values.put(InventoryEntry.COLUMN_PRICE, 55);
+        values.put(InventoryEntry.COLUMN_QUANTITY, 20);
+        values.put(InventoryEntry.COLUMN_SUPPLIER_NAME, "Guma proizvod");
+        values.put(InventoryEntry.COLUMN_SUPPLIER_WEB, "www.google.com");
+        values.put(InventoryEntry.COLUMN_SUPPLIER_EMAIL, "mail@me.com");
+        values.put(InventoryEntry.COLUMN_PICTURE, byteArray);
+
+        // insert it into items table
+        long rowId = db.insert(InventoryEntry.TABLE_NAME, null, values);
+
+        if (rowId == -1){
+            Toast.makeText(this, "Error inserting item", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Successfully entered item", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
